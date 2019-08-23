10005d74d3e40786458f1e92d6de55875374a975
Jack O'Sullivan <jackos1998@gmail.com>
2019-08-23 13:01:28 +0100

    Add 404 and server error handlers

--- net-dhcp/__init__.py
+++ net-dhcp/__init__.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 from . import network, ipam
+
+@app.errorhandler(404)
+def err_not_found(e):
+    return jsonify({'Err': 'API not found'}), 404
+
+@app.errorhandler(Exception)
+def err(e):
+    return jsonify({'Err': f'Error: {e}'}), 500
