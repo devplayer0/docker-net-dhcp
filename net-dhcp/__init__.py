895e9aa9c48c0c357e7a356abd11eef49e356cde
Jack O'Sullivan <jackos1998@gmail.com>
2019-08-23 14:38:13 +0100

    Log exceptions

--- net-dhcp/__init__.py
+++ net-dhcp/__init__.py
@@ -1,8 +1,12 @@
+import logging
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 from . import network
+
+logger = logging.getLogger('gunicorn.error')
 
 @app.errorhandler(404)
 def err_not_found(e):
@@ -10,4 +14,5 @@ def err_not_found(e):
 
 @app.errorhandler(Exception)
 def err(e):
+    logger.exception(e)
     return jsonify({'Err': f'Error: {e}'}), 500
