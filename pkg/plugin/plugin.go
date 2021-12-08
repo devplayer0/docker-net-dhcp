1bb0ffe9f27531b8ec94a39788b1e7ccf6588ab0
Colin Findlay <colin@flywheel.nz>
2021-12-09 10:36:07 +1300

    Add timeout on connection to Docker

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -76,7 +76,10 @@ type Plugin struct {
 
 // NewPlugin creates a new Plugin
 func NewPlugin(awaitTimeout time.Duration) (*Plugin, error) {
-	client, err := docker.NewClient("unix:///run/docker.sock", "v1.13.1", nil, nil)
+	client, err := docker.NewClientWithOpts(
+		docker.WithHost("unix:///run/docker.sock"),
+		docker.WithVersion("v1.13.1"),
+		docker.WithTimeout(2*time.Second)) // If local Docker doesn't respond in under 2s, it's probably not ready.
 	if err != nil {
 		return nil, fmt.Errorf("failed to create docker client: %w", err)
 	}
